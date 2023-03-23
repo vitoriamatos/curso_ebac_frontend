2d7a6c1c98a6db30c1e7cfe082b6b9ae2b2a4981
vitoriamatos <vitoria.matos99@gmail.com>
2023-03-23 12:36:27 -0300

    erro

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-$(document).ready(function () {
-
+$(document).ready(function(){
+    
     const endpoint = `https://api.github.com/users/vitoriamatos`
-
+    
 
     fetch(endpoint)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (json) {
-            const repositorys = json.public_repos;
-            const followers = json.followers;
-            const following = json.following;
-            const name = json.name;
-            const login = json.login;
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+        const repositorys = json.public_repos;
+        const followers = json.followers;
+        const following = json.following;
+        const name = json.name;
+        const login = json.login;
 
-            $('#repository').text(repositorys);
-            $('#followers').text(followers);
-            $('#following').text(following);
-            $('#name').text(name);
-            $('#login').text(`@${login}`);
-        })
+        $('#repository').text(repositorys);
+        $('#followers').text(followers);
+        $('#following').text(following);
+        $('#name').text(name);
+        $('#login').text(`@${login}`);
+    })
+    .catch(function(erro){
+        alert("Ocorreu um erro ao buscar as informações.");
+    })
+
+
 })
