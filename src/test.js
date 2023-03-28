0fd846ee0a9ef7e8612fa45ee0fa6053a9309a06
vitoriamatos <vitoria.matos99@gmail.com>
2023-03-28 12:04:06 -0300

    first commit!

---
+++ src/test.js
@@ -0,0 +1,5 @@
+"use strict";
+const mult = (number1, number2) => number1 * number2;
+const salute = (name) => "Olá " + name;
+console.log(mult(1,2));
+console.log(salute('naruto'));
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+const mult = (number1: number, number2: number): number => number1 * number2;
+
+const salute = (name: string): string => "Olá " + name;
+
+console.log(mult);
+
+console.log(salute);
